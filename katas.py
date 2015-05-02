37aefa6f30725f99d77e72555f5a19e2bad1da4e
moriewna <syeeric@gmail.com>
2015-05-02 23:42:04 +0200

    new katas added

--- katas.py
+++ katas.py
@@ -45,3 +45,69 @@ def stock_list(listOfArt, listOfCat):
 b = ["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"]
 c = ["A", "B"]
 #print stock_list(b,c)
+
+
+def dirReduc(arr):
+    return checkDir(checkDir(checkDir(arr)))
+
+def checkDir(arr):
+    par = {"NORTH":"SOUTH","SOUTH":"NORTH","EAST":"WEST","WEST":"EAST"}
+    if len(arr) < 2 :return arr
+    else :
+        if arr[1] == par[arr[0]] : return checkDir(arr[2:])
+        else: return [arr[0]] + checkDir(arr[1:])
+
+
+a = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+u = ["NORTH", "WEST", "SOUTH", "EAST"]
+#print dirReduc(a)
+
+def createDict(keys, values):
+    d = {}
+    for i,k in enumerate(keys):
+        if i < len(values):
+            d[k] = values[i]
+        else :
+            d[k] = None
+    return d
+
+
+keys = ['a', 'b', 'c', 'd']
+values = [1, 2, 3]
+
+#print createDict(keys,values)
+# return dict([(keys[x], values[x]) if x < len(values) else (keys[x], None) for x in range(len(keys))])
+
+def duplicate_encode(word):
+    return "".join(["("if word.lower().count(x) == 1 else ")" for x in word.lower()])
+
+def count_squares(n):
+    if n == 1 : return 1
+    else : return n*n + count_squares(n-1)
+
+#print count_squares(3)
+
+def isValidWalk(walk):
+    return all([walk.count('n')%4 == 0 , walk.count('s')%4 == 0, walk.count('e')%4 == 0, walk.count('w')%4 == 0, walk.count('w') == 10])
+
+# print isValidWalk(['n', 's', 'e', 'w', 'e', 's', 's', 's', 's', 'e', 'w', 'w', 'w', 's', 'e'])
+
+
+def addsup2(a1, a2, a3):
+    return [[v1,v2,v3] for v1 in a1 for v2 in a2 for v3 in a3 if v1+v2==v3]
+
+#print (addsup2([1,2,5],[3,1],[5,4,6]))
+
+
+def f(n, m):
+    return sum([i%int(m) for i in range(1,int(n)+1)])
+
+#print f(5, 88)
+
+def midpoint_sum(ints):
+    if len(ints) <= 2 : return None
+    for i in range(1,len(ints)-1):
+        if sum(ints[0:i]) == sum(ints[i+1:]) : return i
+    return None
+
+#print midpoint_sum([4,1,7,9,3,9])
