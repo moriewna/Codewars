ccfe79a4c44e33e4313a1d4e39f0c69e0449729e
moriewna <syeeric@gmail.com>
2015-05-03 18:58:32 +0200

    New katas + lambdas

---
+++ lambdas.py
@@ -0,0 +1,8 @@
+spoken    = lambda greeting: greeting.title()+"."
+shouted   = lambda greeting: greeting.upper()+"!"
+whispered = lambda greeting: greeting.lower()+"."
+
+greet = lambda style, msg: style(msg)
+
+
+print greet(spoken, "Hello")
